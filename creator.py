3170c49b7306e4eca5b039c0bc7f5fd278a67e3d
Holger <holger.dobosz@dci.education>
2023-04-19 15:39:14 +0200

    Changes

--- creator.py
+++ creator.py
@@ -1,19 +1,19 @@
 import random
-print("*********************************")
+
 print("welcom to the character generater!")
-print("Let's name the brave adventurers:")
-name1=input("character1:")
-name2=input("character2:")
-name3=input("character3:")
-name4=input("character4:")
-name5=input("character5:")
+print("Let's name the brave adventurers: ")
+name1=input("character1: ")
+name2=input("character2: ")
+name3=input("character3: ")
+name4=input("character4: ")
+name5=input("character5: ")
 name1_num= random.randint(1,3)
 name2_num= random.randint(1,3)
 name3_num= random.randint(1,3)
 name4_num= random.randint(1,3)
 name5_num= random.randint(1,3)
-print("Enough name!!!")
-############################name1
+print("***YOUR CHARACTERS ARE COMPLETED!!!***")
+
 if (name1_num == 1):
     print(name1,"is a Wizard")
     health=random.randint(5,10)
@@ -22,7 +22,7 @@ if (name1_num == 1):
     print("strength =",strength)
     magic=3*random.randint(5,10)
     print("magic is =" ,magic)
-if (name1_num == 2):
+elif (name1_num == 2):
     print(name2,"is a Warrior")
     health=random.randint(5,10)
     print("health =" ,health)
@@ -30,7 +30,7 @@ if (name1_num == 2):
     print("strength =",strength)
     magic=random.randint(5,10)
     print("magic is =" ,magic)
-if (name1_num == 3):
+elif (name1_num == 3):
     print(name3,"is a potato")
     health=random.randint(5,10)
     print("health =" ,3*health)
@@ -38,3 +38,28 @@ if (name1_num == 3):
     print("strength =",strength)
     magic=random.randint(5,10)
     print("magic is =" ,magic)
+
+if (name2_num == 1):
+    print(name1,"is a Wizard")
+    health=random.randint(5,10)
+    print("health =" ,health)
+    strength=random.randint(5,10)
+    print("strength =",strength)
+    magic=3*random.randint(5,10)
+    print("magic is =" ,magic)
+elif (name2_num == 2):
+    print(name2,"is a Warrior")
+    health=random.randint(5,10)
+    print("health =" ,health)
+    strength=3*random.randint(5,10)
+    print("strength =",strength)
+    magic=random.randint(5,10)
+    print("magic is =" ,magic)
+elif (name2_num == 3):
+    print(name3,"is a potato")
+    health=random.randint(5,10)
+    print("health =" ,3*health)
+    strength=random.randint(5,10)
+    print("strength =",strength)
+    magic=random.randint(5,10)
+    print("magic is =" ,magic)  
